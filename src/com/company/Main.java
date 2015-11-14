9cf9b75eae28e26089f75c164f3f22fa552aa0b9
tulipan <modrzejewskimichal@wp.pl>
2015-11-14 13:13:20 +0100

    kommit1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,19 +13,24 @@ public class Main {
         //xjeden=(-b-pierwiastekdelta)/(2*a)
         //xdwa=(-b+pierwiastekdelta)/(2*a)
         // write your code here
-        double a=2, b=-3, c=-8;
+        Scanner wej = new Scanner(System.in);
+        System.out.println("Podaj zmienną a");
+        double a = wej.nextInt();
+        System.out.println("Podaj zmienną b");
+        double b = wej.nextInt();
+        System.out.println("Podaj zmienną c");
+        double c = wej.nextInt();
         double x1, x2, x0, d;
-        d=b*b-4*a*c;
-        if(d<0) {
-            x1=(-b- Math.sqrt(d))/(2*a);
-            x2=(-b+ Math.sqrt(d))/(2*a);
+        d = b * b - 4 * a * c;
+        if (d < 0) {
+            x1 = (-b - Math.sqrt(d)) / (2 * a);
+            x2 = (-b + Math.sqrt(d)) / (2 * a);
             System.out.println("x1" + x1);
             System.out.println("x2" + x2);
-        }
-        else if(d>0){
+        } else if (d > 0) {
             System.out.println("x nie istnieje");
-        }
-        else if(d==0){x0=-b/2*a;
+        } else if (d == 0) {
+            x0 = -b / 2 * a;
             System.out.println(x0);
 
         }
